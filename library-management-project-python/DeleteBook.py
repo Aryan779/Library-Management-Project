6266542866032d0e808a4803d63f6987de8bdb40
Aryan Patel <84985729+Aryan779@users.noreply.github.com>
2022-10-19 20:00:22 +0530

    Add files via upload

---
+++ library-management-project-python/DeleteBook.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from tkinter import messagebox
+import pymysql
+
+
+mysql = pymysql.connect(host="localhost",user="root",password="Aryanpatel@2021",database="db1")
+cur = mysql.cursor()
+
+issueTable = "books_issued" 
+bookTable = "books"
+
+
+def deleteBook():
+    
+    bid = bookInfo1.get()
+    
+    deleteSql = "delete from "+bookTable+" where bid = '"+bid+"'"
+    deleteIssue = "delete from "+issueTable+" where bid = '"+bid+"'"
+    try:
+        cur.execute(deleteSql)
+        mysql.commit()
+        cur.execute(deleteIssue)
+        mysql.commit()
+        messagebox.showinfo('Success',"Book Record Deleted Successfully")
+    except:
+        messagebox.showinfo("Please check Book ID")
+    
+
+    print(bid)
+
+    bookInfo1.delete(0, END)
+    root.destroy()
+    
+def delete(): 
+    
+    global bookInfo1,mysql,cur,bookTable,root
+    
+    root = Tk()
+    root.title("Library Management Sytem")
+    root.minsize(width=400,height=400)
+    root.geometry("600x500")
+
+
+        
+    headingFrame1 = Frame(root,bd=5)
+    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
+        
+    headingLabel = Label(headingFrame1, text="Delete Book", bg='black', fg='white', font=('Courier',15))
+    headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+    
+    labelFrame = Frame(root,bg='black')
+    labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)   
+        
+    # Book ID to Delete
+    lb2 = Label(labelFrame,text="Book ID : ", bg='black', fg='white')
+    lb2.place(relx=0.05,rely=0.5)
+        
+    bookInfo1 = Entry(labelFrame)
+    bookInfo1.place(relx=0.3,rely=0.5, relwidth=0.62)
+    
+    #Submit Button
+    SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=deleteBook)
+    SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+    quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
+    quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+    root.mainloop()
---
+++ library-management-project-python/main.py
@@ -0,0 +1,39 @@
+from tkinter import *
+import pymysql
+from AddBook import *
+from DeleteBook import *
+from ViewBooks import *
+from IssueBook import *
+from ReturnBook import *
+
+mysql = pymysql.connect(host="localhost",user="root",password="Aryanpatel@2021",database="db1")
+cur = mysql.cursor()
+
+root = Tk()
+root.title("Library Management Sytem")
+root.minsize(width=400,height=400)
+root.geometry("600x500")
+    
+
+headingFrame1 = Frame(root,bd=5)
+headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
+
+headingLabel = Label(headingFrame1, text="Library Management System", bg='black', fg='white', font=('Courier',15))
+headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+
+btn1 = Button(root,text="Add Book Details",bg='black', fg='white', command=addBook)
+btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
+    
+btn2 = Button(root,text="Delete Book",bg='black', fg='white', command=delete)
+btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
+    
+btn3 = Button(root,text="View Book List",bg='black', fg='white', command=View)
+btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
+    
+btn4 = Button(root,text="Issue Book to Student",bg='black', fg='white', command = issueBook)
+btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
+    
+btn5 = Button(root,text="Return Book",bg='black', fg='white', command = returnBook)
+btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
+
+root.mainloop()
